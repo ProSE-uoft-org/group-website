f9acf372ab20166e48b600e17073f2b72b333869
Huakun Shen <huakun.shen@huakunshen.com>
2023-10-28 12:04:04 -0400

    Modify script for extracting tar.gz static build to target dir

--- scripts/extract_to_public_html.sh
+++ scripts/extract_to_public_html.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ASSET_NAME="static_website.tar.gz"
-tar -zxvf $ASSET_NAME ~/public_html
+tar -zxvf $ASSET_NAME
+# clear ~/public_html
+rm -rf ~/public_html/*
+# move the contents of the extracted tarball to ~/public_html
+mv static_website/* ~/public_html
