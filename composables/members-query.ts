c5ec8e7b90324ab9f2710f4101af2316d4229b5a
Huakun Shen <huakun.shen@huakunshen.com>
2023-10-30 00:56:25 -0400

    Update some page text

--- composables/members-query.ts
+++ composables/members-query.ts
@@ -1,6 +1,6 @@
 export const useAllFacultyMemberQuery = () =>
   queryContent("members", "faculty");
-export const useAllPostDocQuery = () => queryContent("members", "postdoc");
+export const useAllStaffAndPostDocQuery = () => queryContent("members", "staff-n-postdoc");
 export const useAllGradStudentQuery = () =>
   queryContent("members", "grad-student");
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-// import {useAlumniQuery}
+useHead({
+  title: 'UofT'
+})
+
 const facultyMembers = await useAllFacultyMemberQuery()
   .sort({ name: 1 })
   .find();
-const postDocMembers = await useAllPostDocQuery().sort({ name: 1 }).find();
+const postDocMembers = await useAllStaffAndPostDocQuery().sort({ name: 1 }).find();
 const gradStudents = await useAllGradStudentQuery().sort({ name: 1 }).find();
 const alumnis = await useAllAlumniQuery().sort({ name: 1 }).find();
 </script>
@@ -12,10 +15,10 @@ const alumnis = await useAllAlumniQuery().sort({ name: 1 }).find();
     <div class="max-w-screen-lg">
       
       <!-- <img src="/UofT.jpeg" /> -->
-      <h1 class="text-ut text-4xl font-bold">PLSE Group</h1>
+      <h1 class="text-ut text-4xl font-bold">University of Toronto PLSE Group</h1>
       <h2 class="text-3xl mt-10">Faculty</h2>
       <PeopleList :people="facultyMembers" />
-      <h2 class="text-3xl mt-10">Post Doc</h2>
+      <h2 class="text-3xl mt-10">Staff and Post Doc</h2>
       <PeopleList :people="postDocMembers" />
       <h2 class="text-3xl mt-10">Grad Student</h2>
       <PeopleList :people="gradStudents" />
