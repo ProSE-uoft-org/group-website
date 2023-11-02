fc310482d1bb42488a9916d81624d377920b61b7
Huakun Shen <huakun.shen@huakunshen.com>
2023-11-02 18:10:54 -0400

    Add robust base url config with image patch

--- components/person.vue
+++ components/person.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
 defineProps<{ name: string; avatar?: string; website?: string }>();
+const config = useRuntimeConfig();
+console.log(config.public.baseURL);
+function composeAvatarUrl(baseUrl: string, avatarUrl: string): string {
+  if (avatarUrl.startsWith("/")) {
+    // image is a local image
+    if (baseUrl.endsWith("/")) {
+      return `${baseUrl.substring(0, baseUrl.length - 1)}${avatarUrl}`;
+    } else {
+      return `${baseUrl}${avatarUrl}`;
+    }
+  } else {
+    // is remote image
+    return avatarUrl;
+  }
+}
 </script>
 <template>
   <div class="flex justify-center p-5">
@@ -7,7 +22,7 @@ defineProps<{ name: string; avatar?: string; website?: string }>();
       <div class="img-container overflow-hidden relative h-52 w-40">
         <img
           v-if="avatar"
-          :src="avatar"
+          :src="composeAvatarUrl(config.public.baseURL, avatar)"
           alt=""
           class="rounded-lg object-cover object-top w-full h-full"
         />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,10 @@
+import { z } from "zod";
+
+// in dev mode, no custom prefix, in order to properly load
+const defaultBaseUrl = process.env.NODE_ENV === "development" ? "/" : "/~prose"
+// const defaultBaseUrl = "/~prose"
+const baseUrl = z.string().regex(/\/.*/, 'A prefix base url should start with a slash /').default(defaultBaseUrl).parse(process.env.BASE_URL);
+
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: "@nuxt-themes/docus",
@@ -12,10 +19,16 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "nuxt-gtag",
   ],
-  // app: {
-  //   baseURL: '/~prose'
-  // },
-  // or use NUXT_APP_BASE_URL=/~huakunshen/ npm run generate
+  app: {
+    // https://nuxt.com/docs/api/nuxt-config#baseurl
+    // or use NUXT_APP_BASE_URL=/~huakunshen/ npm run generate
+    baseURL: baseUrl,
+  },
+  runtimeConfig: {
+    public: {
+      baseURL: baseUrl
+    }
+  },
   gtag: {
     // The Google Analytics 4 property ID to use for tracking
     id: "G-887SRCJHXZ",
