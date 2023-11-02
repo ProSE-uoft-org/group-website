71b60e003a4d3d05e6345218a4278c4347419fc2
Huakun Shen <huakun.shen@huakunshen.com>
2023-11-02 17:43:09 -0400

    Add base url config to cicd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
+  extends: "@nuxt-themes/docus",
   devtools: { enabled: true },
   ssr: true,
   modules: [
@@ -12,12 +12,16 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "nuxt-gtag",
   ],
+  // app: {
+  //   baseURL: '/~prose'
+  // },
+  // or use NUXT_APP_BASE_URL=/~huakunshen/ npm run generate
   gtag: {
     // The Google Analytics 4 property ID to use for tracking
     id: "G-887SRCJHXZ",
     // Additional configuration for the Google Analytics 4 property
     config: {
-      page_title: 'UT-SE-Group-Website'
-    }
-  }
-})
+      page_title: "UT-SE-Group-Website",
+    },
+  },
+});
