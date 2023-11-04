8f2f0cc8a36984dbf5d540e487833373c58f039c
Huakun Shen <huakun.shen@huakunshen.com>
2023-11-04 04:19:06 -0400

    Fix gh-pages by setting default base url back to /

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 import { z } from "zod";
 
 // in dev mode, no custom prefix, in order to properly load
-const defaultBaseUrl = process.env.NODE_ENV === "development" ? "/" : "/~prose"
-// const defaultBaseUrl = "/~prose"
-const baseUrl = z.string().regex(/\/.*/, 'A prefix base url should start with a slash /').default(defaultBaseUrl).parse(process.env.BASE_URL);
+const baseUrl = z.string().regex(/\/.*/, 'A prefix base url should start with a slash /').default('/').parse(process.env.BASE_URL);
 
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
