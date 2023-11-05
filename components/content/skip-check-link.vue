a3a06c0b9474f6d10e1363adc3dfaf5f872bd721
Huakun Shen <huakun.shen@huakunshen.com>
2023-11-05 00:55:00 -0400

    Fix link problem caused by subpath.

--- components/content/skip-check-link.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-defineProps<{ url: string }>();
-</script>
-<template>
-  <a :href="url" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{{ url }}</a>
-</template>
--- components/person.vue
+++ components/person.vue
@@ -1,27 +1,33 @@
 <script setup lang="ts">
 defineProps<{ name: string; avatar?: string; website?: string }>();
 const config = useRuntimeConfig();
-function composeAvatarUrl(baseUrl: string, avatarUrl: string): string {
-  if (avatarUrl.startsWith("/")) {
+
+/**
+ * When a subpath is used as base url, relative paths used in this site will break, like links to avatar and markdown files.
+ * This function will fix it by adding baseUrl if it's a local relative path link.
+ */
+function composeUrl(baseUrl: string, url: string): string {
+  if (url.startsWith("/")) {
     // image is a local image
     if (baseUrl.endsWith("/")) {
-      return `${baseUrl.substring(0, baseUrl.length - 1)}${avatarUrl}`;
+      return `${baseUrl.substring(0, baseUrl.length - 1)}${url}`;
     } else {
-      return `${baseUrl}${avatarUrl}`;
+      return `${baseUrl}${url}`;
     }
   } else {
     // is remote image
-    return avatarUrl;
+    return url;
   }
 }
+
 </script>
 <template>
   <div class="flex justify-center p-5">
-    <NuxtLink class="flex flex-col" :href="website" target="_blank">
+    <NuxtLink class="flex flex-col" :href="composeUrl(config.public.baseURL, website)" target="_blank">
       <div class="img-container overflow-hidden relative h-52 w-40">
         <img
           v-if="avatar"
-          :src="composeAvatarUrl(config.public.baseURL, avatar)"
+          :src="composeUrl(config.public.baseURL, avatar)"
           alt=""
           class="rounded-lg object-cover object-top w-full h-full"
         />
